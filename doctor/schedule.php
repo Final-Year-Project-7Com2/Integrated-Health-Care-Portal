f120135dbee03fe6918ec032a72af31fe5a029af
Azahar2912 <150232963+Azahar2912@users.noreply.github.com>
2023-12-06 16:15:13 +0530

    Updated schedule.php

--- doctor/schedule.php
+++ doctor/schedule.php
@@ -129,126 +129,95 @@ $list110 = $database->query("select  * from  schedule where docid=$userid;");
 <td width="30%">
 <form action="" method="post">
  <input type="date" name="sheduledate" id="date" class="input-text filter-container-items" style="margin: 0;width: 95%;">
+</td>
+ <td width="12%">
+<input type="submit"  name="filter" value=" Filter" class=" btn-primary-soft btn button-icon btn-filter"  style="padding: 15px; margin :0;width:100%">
+ </form>
+</td>
+</tr>
+ </table>
+ </center>
+</td>
+</tr>
+ <?php
+$sqlmain= "select schedule.scheduleid,schedule.title,doctor.docname,schedule.scheduledate,schedule.scheduletime,schedule.nop from schedule inner join doctor on schedule.docid=doctor.docid where doctor.docid=$userid ";
+  if($_POST){
+           $sqlpt1="";
+       if(!empty($_POST["sheduledate"])){
+            $sheduledate=$_POST["sheduledate"];
+             $sqlmain.=" and schedule.scheduledate='$sheduledate' ";
+   }
 
-                        </td>
-                        
-                    <td width="12%">
-                        <input type="submit"  name="filter" value=" Filter" class=" btn-primary-soft btn button-icon btn-filter"  style="padding: 15px; margin :0;width:100%">
-                        </form>
-                    </td>
-
-                    </tr>
-                            </table>
-
-                        </center>
-                    </td>
-                    
-                </tr>
-                
-                <?php
-
-                $sqlmain= "select schedule.scheduleid,schedule.title,doctor.docname,schedule.scheduledate,schedule.scheduletime,schedule.nop from schedule inner join doctor on schedule.docid=doctor.docid where doctor.docid=$userid ";
-                    if($_POST){
-                        //print_r($_POST);
-                        $sqlpt1="";
-                        if(!empty($_POST["sheduledate"])){
-                            $sheduledate=$_POST["sheduledate"];
-                            $sqlmain.=" and schedule.scheduledate='$sheduledate' ";
-                        }
-
-                    }
-
-                ?>
-                  
-                <tr>
-                   <td colspan="4">
-                       <center>
-                        <div class="abc scroll">
-                        <table width="93%" class="sub-table scrolldown" border="0">
-                        <thead>
-                        <tr>
-                                <th class="table-headin">
-                                    
-                                
-                                Session Title
-                                
-                                </th>
-                                
-                                
-                                <th class="table-headin">
-                                    
-                                    Sheduled Date & Time
-                                    
-                                </th>
-                                <th class="table-headin">
-                                    
-                                Max num that can be booked
-                                    
-                                </th>
-                                
-                                <th class="table-headin">
-                                    
-                                    Events
-                                    
-                                </tr>
-                        </thead>
-                        <tbody>
-                        
-                            <?php
-
-                                
-                                $result= $database->query($sqlmain);
-
-                                if($result->num_rows==0){
-                                    echo '<tr>
-                                    <td colspan="4">
-                                    <br><br><br><br>
-                                    <center>
-                                    <img src="../img/notfound.svg" width="25%">
-                                    
-                                    <br>
-                                    <p class="heading-main12" style="margin-left: 45px;font-size:20px;color:rgb(49, 49, 49)">We  couldnt find anything related to your keywords !</p>
-                                    <a class="non-style-link" href="schedule.php"><button  class="login-btn btn-primary-soft btn"  style="display: flex;justify-content: center;align-items: center;margin-left:20px;">&nbsp; Show all Sessions &nbsp;</font></button>
-                                    </a>
-                                    </center>
-                                    <br><br><br><br>
-                                    </td>
-                                    </tr>';
-                                    
-                                }
-                                else{
-                                for ( $x=0; $x<$result->num_rows;$x++){
-                                    $row=$result->fetch_assoc();
-                                    $scheduleid=$row["scheduleid"];
-                                    $title=$row["title"];
-                                    $docname=$row["docname"];
-                                    $scheduledate=$row["scheduledate"];
-                                    $scheduletime=$row["scheduletime"];
-                                    $nop=$row["nop"];
-                                    echo '<tr>
-                                        <td> &nbsp;'.
-                                        substr($title,0,30)
-                                        .'</td>
-                                        
-                                        <td style="text-align:center;">
-                                            '.substr($scheduledate,0,10).' '.substr($scheduletime,0,5).'
-                                        </td>
-                                        <td style="text-align:center;">
-                                            '.$nop.'
-                                        </td>
-
-                                        <td>
-                                        <div style="display:flex;justify-content: center;">
-                                        
-                                        <a href="?action=view&id='.$scheduleid.'" class="non-style-link"><button  class="btn-primary-soft btn button-icon btn-view"  style="padding-left: 40px;padding-top: 12px;padding-bottom: 12px;margin-top: 10px;"><font class="tn-in-text">View</font></button></a>
-                                       &nbsp;&nbsp;&nbsp;
-                                       <a href="?action=drop&id='.$scheduleid.'&name='.$title.'" class="non-style-link"><button  class="btn-primary-soft btn button-icon btn-delete"  style="padding-left: 40px;padding-top: 12px;padding-bottom: 12px;margin-top: 10px;"><font class="tn-in-text">Cancel Session</font></button></a>
-                                        </div>
-                                        </td>
-                                    </tr>';
-                                    
-                                }
-                            }
+   }
+ ?>
+<tr>
+<td colspan="4">
+<center>
+ <div class="abc scroll">
+ <table width="93%" class="sub-table scrolldown" border="0">
+ <thead>
+ <tr>
+ <th class="table-headin">
+                           Session Title
+</th>
+<th class="table-headin">
+                       Sheduled Date & Time
+</th>
+ <th class="table-headin">
+             Max num that can be booked
+ </th>
+  <th class="table-headin">
+                       Events
+ </tr>
+ </thead>
+  <tbody>
+  <?php
+ $result= $database->query($sqlmain);
+if($result->num_rows==0){
+        echo '<tr>
+     <td colspan="4">
+     <br><br><br><br>
+     <center>
+     img src="../img/notfound.svg" width="25%">
+ <br>
+  <p class="heading-main12" style="margin-left: 45px;font-size:20px;color:rgb(49, 49, 49)">We  couldnt find anything related to your keywords !</p>
+  <a class="non-style-link" href="schedule.php"><button  class="login-btn btn-primary-soft btn"  style="display: flex;justify-content: center;align-items: center;margin-left:20px;">&nbsp; Show all Sessions &nbsp;</font></button>
+  </a>
+ </center>
+ <br><br><br><br>
+ </td>
+ </tr>';
+}
+else{
+     for ( $x=0; $x<$result->num_rows;$x++){
+            $row=$result->fetch_assoc();
+            $scheduleid=$row["scheduleid"];
+            $title=$row["title"];
+            $docname=$row["docname"];
+            $scheduledate=$row["scheduledate"];
+            $scheduletime=$row["scheduletime"];
+            $nop=$row["nop"];
+echo '<tr>
+<td> &nbsp;'.
+      substr($title,0,30)
+.'</td>
+<td style="text-align:center;">
+'.substr($scheduledate,0,10).' '.substr($scheduletime,0,5).'
+</td>
+<td style="text-align:center;">
+'.$nop.'
+</td>
+<td>
+<div style="display:flex;justify-content: center;">
+<a href="?action=view&id='.$scheduleid.'" class="non-style-link"><button  class="btn-primary-soft btn button-icon btn-view"  style="padding-left: 40px;padding-top: 12px;padding-bottom: 12px;margin-top: 10px;"><font class="tn-in-text">View</font></button></a>
+&nbsp;&nbsp;&nbsp;
+<a href="?action=drop&id='.$scheduleid.'&name='.$title.'" class="non-style-link"><button  class="btn-primary-soft btn button-icon btn-delete"  style="padding-left: 40px;padding-top: 12px;padding-bottom: 12px;margin-top: 10px;"><font class="tn-in-text">Cancel Session</font></button></a>
+</div>
+</td>
+</tr>';
+}
+ 
+}
 ?>
  
  </tbody>
