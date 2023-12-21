499aa83f17e93f4d2515ba20bfd25726cd3711c4
Akashm3112 <150229674+akashm3112@users.noreply.github.com>
2023-12-21 17:24:30 +0530

    Update appointment.php

--- patient/appointment.php
+++ patient/appointment.php
@@ -173,3 +173,234 @@ Date:
 <div class="abc scroll">
 <table width="93%" class="sub-table scrolldown" border="0" style="border:none">
 <tbody>
+        <?php
+ if($result->num_rows==0){
+echo '<tr>
+<td colspan="7">
+<br><br><br><br>
+<center>
+<img src="../img/notfound.svg" width="25%">
+<br>
+     <p class="heading-main12" style="margin-left: 45px;font-size:20px;color:rgb(49, 49, 49)">We  couldnt find anything related to your keywords !</p>
+                 <a class="non-style-link" href="appointment.php"><button  class="login-btn btn-primary-soft btn"  style="display: flex;justify-content: center;align-items: center;margin-left:20px;">&nbsp; Show all Appointments &nbsp;</font></button>
+             </a>
+  </center>
+<br><br><br><br>
+</td>
+</tr>';
+      }
+else{
+ for ( $x=0; $x<($result->num_rows);$x++){
+   echo "<tr>";
+   for($q=0;$q<3;$q++){
+$row=$result->fetch_assoc();
+if (!isset($row)){
+break;
+};
+$scheduleid=$row["scheduleid"];
+$title=$row["title"];
+$docname=$row["docname"];
+$scheduledate=$row["scheduledate"];
+$scheduletime=$row["scheduletime"];
+$apponum=$row["apponum"];
+$appodate=$row["appodate"];
+$appoid=$row["appoid"];
+    
+if($scheduleid==""){
+break;
+}
+  echo '
+  <td style="width: 25%;">
+<div  class="dashboard-items search-items"  >
+   <div style="width:100%;">
+     <div class="h3-search">
+Booking Date: '.substr($appodate,0,30).'<br>
+Reference Number: OC-000-'.$appoid.'
+</div>
+<div class="h1-search">
+'.substr($title,0,21).'<br>
+</div>
+<div class="h3-search">
+Appointment Number:<div class="h1-search">0'.$apponum.'</div>
+</div>
+<div class="h3-search">
+'.substr($docname,0,30).'
+</div>
+             <div class="h4-search">
+                Scheduled Date: '.$scheduledate.'<br>Starts: <b>@'.substr($scheduletime,0,5).'</b> (24h)
+                 </div>
+                   <br>
+                     <a href="?action=drop&id='.$appoid.'&title='.$title.'&doc='.$docname.'" ><button  class="login-btn btn-primary-soft btn "  style="padding-top:11px;padding-bottom:11px;width:100%"><font class="tn-in-text">Cancel Booking</font></button></a>
+                      </div>
+                 </div>
+             </td>';
+     }
+        echo "</tr>";
+             }
+        }
+        ?>
+ </tbody>
+ </table>
+      </div>
+       </center>
+     </td> 
+   </tr>
+ </table>
+        </div>
+    </div>
+    <?php
+    
+    if($_GET){
+        $id=$_GET["id"];
+        $action=$_GET["action"];
+        if($action=='booking-added'){
+            
+ echo '
+ <div id="popup1" class="overlay">
+<div class="popup">
+<center>
+<br><br>
+<h2>Booking Successfully.</h2>
+<a class="close" href="appointment.php">&times;</a>
+<div class="content">
+Your Appointment number is '.$id.'.<br><br>
+       </div>
+<div style="display: flex;justify-content: center;">
+
+ <a href="appointment.php" class="non-style-link"><button  class="btn-primary btn"  style="display: flex;justify-content: center;align-items: center;margin:10px;padding:10px;"><font class="tn-in-text">&nbsp;&nbsp;OK&nbsp;&nbsp;</font></button></a>
+<br><br><br><br>
+</div>
+</center>
+</div>
+</div>
+';
+}elseif($action=='drop'){
+            $title=$_GET["title"];
+            $docname=$_GET["doc"];
+  echo '
+<div id="popup1" class="overlay">
+<div class="popup">
+<center>
+<h2>Are you sure?</h2>
+<a class="close" href="appointment.php">&times;</a>
+<div class="content">
+You want to Cancel this Appointment?<br><br>
+Session Name: &nbsp;<b>'.substr($title,0,40).'</b><br>
+Doctor name&nbsp; : <b>'.substr($docname,0,40).'</b><br><br>
+                            
+        </div>
+<div style="display: flex;justify-content: center;">
+<a href="delete-appointment.php?id='.$id.'" class="non-style-link"><button  class="btn-primary btn"  style="display: flex;justify-content: center;align-items: center;margin:10px;padding:10px;"<font class="tn-in-text">&nbsp;Yes&nbsp;</font></button></a>&nbsp;&nbsp;&nbsp;
+                        <a href="appointment.php" class="non-style-link"><button  class="btn-primary btn"  style="display: flex;justify-content: center;align-items: center;margin:10px;padding:10px;"><font class="tn-in-text">&nbsp;&nbsp;No&nbsp;&nbsp;</font></button></a>
+
+                        </div>
+                    </center>
+            </div>
+            </div>
+            '; 
+        }elseif($action=='view'){
+            $sqlmain= "select * from doctor where docid=?";
+            $stmt = $database->prepare($sqlmain);
+            $stmt->bind_param("i",$id);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $row=$result->fetch_assoc();
+            $name=$row["docname"];
+            $email=$row["docemail"];
+            $spe=$row["specialties"];
+            
+            $sqlmain= "select sname from specialties where id=?";
+            $stmt = $database->prepare($sqlmain);
+            $stmt->bind_param("s",$spe);
+            $stmt->execute();
+            $spcil_res = $stmt->get_result();
+            $spcil_array= $spcil_res->fetch_assoc();
+            $spcil_name=$spcil_array["sname"];
+            $nic=$row['docnic'];
+            $tele=$row['doctel'];
+            echo '
+            <div id="popup1" class="overlay">
+           <div class="popup">
+          <center>
+<h2></h2>
+<a class="close" href="doctors.php">&times;</a>
+<div class="content">
+eDoc Web App<br>
+   </div>
+<div style="display: flex;justify-content: center;">
+<table width="80%" class="sub-table scrolldown add-doc-form-container" border="0">
+         <tr>
+             <td>
+       <p style="padding: 0;margin: 0;text-align: left;font-size: 25px;font-weight: 500;">View Details.</p><br><br>
+</td>
+</tr>
+<tr>
+      <td class="label-td" colspan="2">
+     <label for="name" class="form-label">Name: </label>
+       </td>
+      </tr>
+   <tr>
+      <td class="label-td" colspan="2">
+           '.$name.'<br><br>
+    </td>
+         </tr>
+    <tr>
+    <td class="label-td" colspan="2">
+<label for="Email" class="form-label">Email: </label>
+</td>
+</tr>
+<tr>
+<td class="label-td" colspan="2">
+'.$email.'<br><br>
+</td>
+</tr>
+<tr>
+<td class="label-td" colspan="2">
+<label for="nic" class="form-label">NIC: </label>
+</td>
+</tr>
+<tr>
+        <td class="label-td" colspan="2">
+'.$nic.'<br><br>
+</td>
+</tr>
+<tr>
+<td class="label-td" colspan="2">
+<label for="Tele" class="form-label">Telephone: </label>
+</td>
+</tr>
+<tr>
+<td class="label-td" colspan="2">
+'.$tele.'<br><br>
+</td>
+</tr>
+<tr>
+<td class="label-td" colspan="2">
+<label for="spec" class="form-label">Specialties: </label>
+        </td>
+      </tr>
+     <tr>
+  <td class="label-td" colspan="2">
+  '.$spcil_name.'<br><br>
+</td>
+  </tr>
+    <tr>
+       <td colspan="2">
+<a href="doctors.php"><input type="button" value="OK" class="login-btn btn-primary-soft btn" ></a>
+      </td>
+    </tr>
+     </table>
+</div>
+</center>
+<br><br>
+</div>
+   </div>
+     ';  
+    }
+}
+
+    ?>
+    </div>
+
+</body>
+</html>
