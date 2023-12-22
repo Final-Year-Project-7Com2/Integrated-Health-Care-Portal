d5ada3c821fd3d2c607e7ea6c1e1358191d0277b
Harsha Vardhan <129319351+harsha987598@users.noreply.github.com>
2023-12-22 08:11:33 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -3,7 +3,7 @@ session_start();
 $_SESSION = array();
 if (isset($_COOKIE[session_name()])) {
 setcookie(session_name(), '', time()-86400, '/');
-}
+} 
 session_destroy();
 header('Location: login.php?action=logout');
 ?>
